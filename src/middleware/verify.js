01f5d280603f400421dddad33b957f4df562b726
Hayk Saakian <hayk@skyrealre.com>
2016-06-03 02:17:36 -0700

    basic verification, untested

--- src/middleware/verify.js
+++ src/middleware/verify.js
@@ -1,10 +1,35 @@
 'use strict';
 
+// HTTP request receives a number of arguments. 
+// POST method is used with application/x-www-form-urlencoded MIME type. 
+// The following arguments are passed to caller:
+
+// call=play
+// addr - client IP address
+// clientid - nginx client id (displayed in log and stat)
+// app - application name
+// flashVer - client flash version
+// swfUrl - client swf url
+// tcUrl - tcUrl
+// pageUrl - client page url
+// name - stream name
+// In addition to the above mentioned items all arguments passed explicitly 
+// to play command are also sent with the callback. For example if stream 
+// is accessed with the url rtmp://localhost/app/movie?a=100&b=face&foo=bar 
+// then a, b & foo are also sent with callback.
+
 module.exports = function(app) {
   return function(req, res, next) {
+    const body = req.body;
+    const streamkey = req.body.name;
+
     console.log(req.originalUrl);
-    res.status(200).send(req.method+' OK')
-    // res.status(403).send('Forbidden')
-    // res.status(404).send('Not Found')
+    app.service('users').find({
+      query: { streamkey: streamkey }
+    })
+    // Then we're good to stream
+    .then(users => res.status(200).send(req.method+' OK'))
+    // On errors, just call our error middleware
+    .catch(() => res.status(403).send('Forbidden'));
   };
 };
