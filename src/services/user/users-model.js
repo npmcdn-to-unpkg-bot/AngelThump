13da8404433b210bf2dd1327dd4e42e5c0ba2a50
root <tim.liang23@gmail.com>
2016-08-05 15:37:19 -0400

    fixed hashing issues becus im an idiot

--- src/services/user/users-model.js
+++ src/services/user/users-model.js
@@ -7,8 +7,6 @@
 
 const mongoose = require('mongoose'),
     Schema = mongoose.Schema,
-    bcrypt = require('bcrypt'),
-    SALT_WORK_FACTOR = 10;
 const validator = require('validator');
 
 const usersSchema = new Schema({
@@ -22,36 +20,6 @@ const usersSchema = new Schema({
   createdAt: { type: Date, 'default': Date.now },
   updatedAt: { type: Date, 'default': Date.now }
 });
-
-usersSchema.pre('save', function(next) {
-    var user = this;
-
-// only hash the password if it has been modified (or is new)
-if (!user.isModified('password')) return next();
-
-// generate a salt
-bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-    if (err) return next(err);
-
-    // hash the password using our new salt
-    bcrypt.hash(user.password, salt, function(err, hash) {
-        if (err) return next(err);
-
-        // override the cleartext password with the hashed one
-        user.password = hash;
-        next();
-    });
-});
-
-
-});
-
-usersSchema.methods.comparePassword = function(candidatePassword, cb) {
-    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-        if (err) return cb(err);
-        cb(null, isMatch);
-    });
-};
 
 
 const usersModel = mongoose.model('users', usersSchema);
