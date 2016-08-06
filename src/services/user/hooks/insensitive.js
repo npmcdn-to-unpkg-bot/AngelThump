f7b2b1dc119cd95226a8d30873e419009de57f47
root <tim.liang23@gmail.com>
2016-08-06 02:39:50 -0400

    delete useless files

--- src/services/user/hooks/insensitive.js
+++ src/services/user/hooks/insensitive.js
@@ -2,15 +2,6 @@
 
 module.exports = function() {
   return function(hook) {
-    // Assign the new data with the username
-    console.log(hook.params.query.username.toLowerCase());
-    /*
-    hook.params.query = Object.assign({}, hook.params.query, {
-      username: hook.params.query.username.toLowerCase()
-    });*/
     hook.params.query.username = hook.params.query.username.toLowerCase();
-
-
-    console.log(hook.params);
   };
 };
--- src/services/user/hooks/test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = function() {
-  return function(hook) {
-    hook.params.query.username = hook.params.query.username.toLowerCase();
-  };
-};
