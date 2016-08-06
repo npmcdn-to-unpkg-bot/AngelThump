ed99ea72ada665d235a077d232412c84c0684e66
root <tim.liang23@gmail.com>
2016-08-06 03:38:40 -0400

    annoying undefined error fixed

--- src/services/user/hooks/insensitive.js
+++ src/services/user/hooks/insensitive.js
@@ -2,6 +2,8 @@
 
 module.exports = function() {
   return function(hook) {
-    hook.params.query.username = hook.params.query.username.toLowerCase();
+    if(typeof hook.params.query.username !== "undefined") {
+    	hook.params.query.username = hook.params.query.username.toLowerCase();
+    }
   };
 };
