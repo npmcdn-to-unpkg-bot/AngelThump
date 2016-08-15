9f468802a79f1f53566533723772decc0bbdd893
Kenan Yildirim <kenan@kenany.me>
2016-08-15 19:26:40 -0400

    make ssl configurable
    Makes the ssl file paths configurable rather than hardcoded. Also allows one to
    disable ssl entirely by simply setting any one of `key`, `cert`, or `ca` to
    `null` (or any other falsey value).
    
    Closes https://github.com/TimIsOverpowered/AngelThump/issues/17

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 'use strict';
 
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const path = require('path');
+
 const app = require('./app');
 const port = app.get('port');
 
-const fs = require('fs');
-const https  = require('https');
+let server;
 
-const server = https.createServer({
-  key: fs.readFileSync('/etc/letsencrypt/live/angelthump.com/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/angelthump.com/cert.pem'),
-  ca: fs.readFileSync('/etc/letsencrypt/live/angelthump.com/fullchain.pem')
-}, app).listen(port);
+// Enable https if paths to key, cert, and ca files has been configured
+if (app.get('key') && app.get('cert') && app.get('ca')) {
+  server = https.createServer({
+    key: fs.readFileSync(path.resolve(__dirname, app.get('key'))),
+    cert: fs.readFileSync(path.resolve(__dirname, app.get('cert'))),
+    ca: fs.readFileSync(path.resolve(__dirname, app.get('ca')))
+  }, app);
+}
 
+// Otherwise fall back to regular http
+else {
+  server = http.createServer(app);
+}
+
+server.listen(port);
 app.setup(server);
